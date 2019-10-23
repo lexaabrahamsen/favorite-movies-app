95aedb2e38c5f432db5b40b5a361193933aa551b
Lexa <lexaraea@gmail.com>
2019-10-23 14:10:03 -0400

    Initial Commit

---
+++ favorite-movies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite-movies-app
+//
+//  Created by Lexa Abrahamsen on 10/23/19.
+//  Copyright © 2019 Lexa Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
